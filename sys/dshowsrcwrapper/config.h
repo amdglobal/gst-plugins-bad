84146ac82ea5d691c419e3fcf4ddaf375bba68f2
Zachary Hueras <zhueras@amdtelemedicine.com>
2019-09-25 17:50:52 -0400

    Updates to build

---
+++ sys/dshowsrcwrapper/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define PACKAGE "gst-plugins-bad"
+#define VERSION "1.16.1"
+#define GST_PACKAGE_NAME "GStreamer Bad Plug-ins AMD"
+#define GST_PACKAGE_ORIGIN "http://gstreamer.freedesktop.org"
+
+#endif
